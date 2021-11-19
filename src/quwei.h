b9b047abb46fa0f9c42a0be82941b65b35b277eb
Weng Xuetian <wengxt@gmail.com>
2021-11-19 04:40:55 -0800

    Add quwei implementation.

--- src/quwei.h
+++ src/quwei.h
@@ -7,18 +7,62 @@
 #ifndef _FCITX5_QUWEI_QUWEI_H_
 #define _FCITX5_QUWEI_QUWEI_H_
 
+#include <fcitx-utils/inputbuffer.h>
 #include <fcitx/addonfactory.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/inputcontext.h>
+#include <fcitx/inputcontextproperty.h>
 #include <fcitx/inputmethodengine.h>
+#include <fcitx/inputpanel.h>
+#include <fcitx/instance.h>
+#include <iconv.h>
+
+class QuweiEngine;
+
+class QuweiState : public fcitx::InputContextProperty {
+public:
+    QuweiState(QuweiEngine *engine, fcitx::InputContext *ic)
+        : engine_(engine), ic_(ic) {}
+
+    void keyEvent(fcitx::KeyEvent &keyEvent);
+    void setCode(int code);
+    void updateUI();
+    void reset() {
+        buffer_.clear();
+        updateUI();
+    }
+
+private:
+    QuweiEngine *engine_;
+    fcitx::InputContext *ic_;
+    fcitx::InputBuffer buffer_{{fcitx::InputBufferOption::AsciiOnly,
+                                fcitx::InputBufferOption::FixedCursor}};
+};
 
 class QuweiEngine : public fcitx::InputMethodEngineV2 {
+public:
+    QuweiEngine(fcitx::Instance *instance);
+
     void keyEvent(const fcitx::InputMethodEntry &entry,
                   fcitx::KeyEvent &keyEvent) override;
+
+    void reset(const fcitx::InputMethodEntry &,
+               fcitx::InputContextEvent &event) override;
+
+    auto factory() const { return &factory_; }
+    auto conv() const { return conv_; }
+    auto instance() const { return instance_; }
+
+private:
+    fcitx::Instance *instance_;
+    fcitx::FactoryFor<QuweiState> factory_;
+    iconv_t conv_;
 };
 
 class QuweiEngineFactory : public fcitx::AddonFactory {
     fcitx::AddonInstance *create(fcitx::AddonManager *manager) override {
         FCITX_UNUSED(manager);
-        return new QuweiEngine;
+        return new QuweiEngine(manager->instance());
     }
 };
 
